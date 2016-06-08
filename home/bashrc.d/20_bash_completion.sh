5e0939bc1b7ad0bfa2e7e88afb5630b3eaa2fdf8
Marcos Klein <mklein0@gmail.com>
2016-06-08 11:42:50 -0700

    Fix check for brew to not fail if brew does not exist.

--- home/bashrc.d/20_bash_completion.sh
+++ home/bashrc.d/20_bash_completion.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/false
 
 # If Mac, check for brew
-type brew 1>/dev/null 2>/dev/null
-if [ $? -eq 0 -a -f $(brew --prefix)/etc/bash_completion ]; then
-	. $(brew --prefix)/etc/bash_completion
-
+if type brew 1>/dev/null 2>/dev/null; then
+	if [ -f $(brew --prefix)/etc/bash_completion ]; then
+		. $(brew --prefix)/etc/bash_completion
+	fi
 elif [ ! -r /etc/bash_completion ]; then
 	if [ -z "${BASH_COMPLETION_DIR}" ]; then
 		# No bash completion run ours
